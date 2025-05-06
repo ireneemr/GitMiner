256a5c45f8dca6a25bfb96f7e864a8881854d387
FIRST_NAME LAST_NAME <dmenae@est.ups.edu.ec>
2025-05-06 15:02:40 +0200

    Commit 2.1, CREACION METODOS PUT, POST y DELETE EN EL PROJECTCONTROLLER

--- src/main/java/aiss/gitminer/controller/ProjectController.java
+++ src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -1,14 +1,14 @@
 package aiss.gitminer.controller;
 
+import aiss.gitminer.exception.CommentNotFoundException;
 import aiss.gitminer.exception.ProjectNotFoundException;
 import aiss.gitminer.model.Project;
 import aiss.gitminer.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,6 +37,42 @@ public class ProjectController {
         }
 
         return project.get();
+    }
+
+    // POST http://localhost:8080/api/projects
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Project createProject(@RequestBody @Valid Project project) {
+        Project _proj = projectRepository.save(project);
+        return _proj;
+    }
+
+    // PUT http://localhost:8080/api/projects/{id}
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProject(@RequestBody @Valid Project upProject, @PathVariable String id) throws ProjectNotFoundException {
+        Optional<Project> projectDATA = projectRepository.findById(id);
+        if(!projectDATA.isPresent()) {
+            throw new ProjectNotFoundException();
+        }
+
+        Project _proj = projectDATA.get();
+        _proj.setName(upProject.name);
+        _proj.setWebUrl(upProject.webUrl);
+
+        projectRepository.save(_proj);
+
+    }
+
+
+    // DELETE http://localhost:8080/api/projects/{id}
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void  deleteProject(@PathVariable String id) throws ProjectNotFoundException {
+        if(!projectRepository.existsById(id)){
+            throw new ProjectNotFoundException();
+        }
+        projectRepository.deleteById(id);
     }
 
 
