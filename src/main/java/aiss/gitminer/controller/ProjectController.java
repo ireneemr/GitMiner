09bc2739fbf181ac563e7a8189bf783962528f12
FIRST_NAME LAST_NAME <dmenae@est.ups.edu.ec>
2025-05-06 14:31:20 +0200

    Commit 2, CREACION METODOS GET EN EL PROJECTCONTROLLER

--- src/main/java/aiss/gitminer/controller/ProjectController.java
+++ src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -1,4 +1,43 @@
 package aiss.gitminer.controller;
 
+import aiss.gitminer.exception.ProjectNotFoundException;
+import aiss.gitminer.model.Project;
+import aiss.gitminer.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/projects")
 public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+
+    // GET http://localhost:8080/api/projects
+    @GetMapping
+    public List<Project> findAll() {
+        return projectRepository.findAll();
+    }
+
+    // GET http://localhost:8080/api/projects/{id}
+    @GetMapping("/{id}")
+    public Project findOne(@PathVariable String id) throws ProjectNotFoundException {
+
+        Optional<Project> project = projectRepository.findById(id);
+
+        if(!project.isPresent()) {
+            throw new ProjectNotFoundException();
+        }
+
+        return project.get();
+    }
+
+
 }
